eba0fde620a4e711b11076e380ebc417695db7ef
vit <viktorr1975@yandex.com>
2023-03-10 20:58:41 +0700

    Сделал login.html

--- django_proj/django_proj/settings.py
+++ django_proj/django_proj/settings.py
@@ -127,9 +127,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_ROOT='/'
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # DRF
